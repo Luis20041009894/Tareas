564fa9a5c1e1bd47f8ed74f0d3cce266ab30041b
Luis20041009894 <hakudoushy19@hotmail.com>
2023-02-03 20:37:21 -0600

    Primer commit
    Tarea1

---
+++ Parcial I/Ejercicio 1/IParcial/IParcial/Tarea_1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace IParcial
+{
+    partial class Tarea_1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Numero1TextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ParoImparTextBox = new System.Windows.Forms.TextBox();
+            this.PosNegaTextBox = new System.Windows.Forms.TextBox();
+            this.LimpiarButton = new System.Windows.Forms.Button();
+            this.SalirButton = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(73, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Inrese número:";
+            // 
+            // Numero1TextBox
+            // 
+            this.Numero1TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Numero1TextBox.Location = new System.Drawing.Point(229, 124);
+            this.Numero1TextBox.Name = "Numero1TextBox";
+            this.Numero1TextBox.ShortcutsEnabled = false;
+            this.Numero1TextBox.Size = new System.Drawing.Size(151, 27);
+            this.Numero1TextBox.TabIndex = 1;
+            this.Numero1TextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Numero1TextBox_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(229, 299);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(235, 43);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Verificar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(93, 178);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Par o Impar:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(41, 233);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Postivo o Negativo:";
+            // 
+            // ParoImparTextBox
+            // 
+            this.ParoImparTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ParoImparTextBox.Location = new System.Drawing.Point(229, 178);
+            this.ParoImparTextBox.Name = "ParoImparTextBox";
+            this.ParoImparTextBox.ReadOnly = true;
+            this.ParoImparTextBox.Size = new System.Drawing.Size(151, 27);
+            this.ParoImparTextBox.TabIndex = 5;
+            // 
+            // PosNegaTextBox
+            // 
+            this.PosNegaTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PosNegaTextBox.Location = new System.Drawing.Point(229, 226);
+            this.PosNegaTextBox.Name = "PosNegaTextBox";
+            this.PosNegaTextBox.ReadOnly = true;
+            this.PosNegaTextBox.Size = new System.Drawing.Size(151, 27);
+            this.PosNegaTextBox.TabIndex = 6;
+            // 
+            // LimpiarButton
+            // 
+            this.LimpiarButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LimpiarButton.Location = new System.Drawing.Point(229, 364);
+            this.LimpiarButton.Name = "LimpiarButton";
+            this.LimpiarButton.Size = new System.Drawing.Size(235, 43);
+            this.LimpiarButton.TabIndex = 7;
+            this.LimpiarButton.Text = "Limpiar";
+            this.LimpiarButton.UseVisualStyleBackColor = true;
+            this.LimpiarButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // SalirButton
+            // 
+            this.SalirButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SalirButton.Location = new System.Drawing.Point(522, 364);
+            this.SalirButton.Name = "SalirButton";
+            this.SalirButton.Size = new System.Drawing.Size(235, 43);
+            this.SalirButton.TabIndex = 8;
+            this.SalirButton.Text = "Salir";
+            this.SalirButton.UseVisualStyleBackColor = true;
+            this.SalirButton.Click += new System.EventHandler(this.SalirButton_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Tarea_1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(903, 546);
+            this.Controls.Add(this.SalirButton);
+            this.Controls.Add(this.LimpiarButton);
+            this.Controls.Add(this.PosNegaTextBox);
+            this.Controls.Add(this.ParoImparTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Numero1TextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Tarea_1";
+            this.Text = "Tarea_1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Numero1TextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox ParoImparTextBox;
+        private System.Windows.Forms.TextBox PosNegaTextBox;
+        private System.Windows.Forms.Button LimpiarButton;
+        private System.Windows.Forms.Button SalirButton;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
---
+++ Parcial I/Ejercicio 1/IParcial/IParcial/Tarea_1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IParcial
+{
+    public partial class Tarea_1 : Form
+    {
+        public Tarea_1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+         
+            //Validaciones
+
+            if (Numero1TextBox.Text == "")
+            {
+                errorProvider1.SetError(Numero1TextBox, "Favor ingresar número");
+                
+                return;
+            
+            } 
+            
+            
+            
+            //Variable 
+            //decimal numero1 = Convert.ToInt32(Numero1TextBox.Text);
+
+            int numero = 0;
+
+            numero = Convert.ToInt32(Numero1TextBox.Text);
+
+
+            ParoImparTextBox.Text = evaluarnumero(numero);
+            PosNegaTextBox.Text = evaluarnumeroneg(numero);
+
+
+
+        }
+        //Función #1
+        private string evaluarnumero(decimal numero)
+        {
+            ///// Par o Impar
+            if (numero % 2 == 0)
+            {
+             return "Numero Par";
+
+            }
+
+            return "Numero Impar";
+                     
+        }
+
+        //Función #2
+
+        private string evaluarnumeroneg(decimal numero)
+        {
+
+            if (numero >= 0)
+            {
+                return "Positivo";
+
+            }
+
+            return "Negativo";
+
+        
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ParoImparTextBox.Clear();
+            PosNegaTextBox.Clear();
+            Numero1TextBox.Clear();
+
+           
+        }
+
+        private void SalirButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        //Para validar solo numeros o ((usar expresiones regulares))
+        private void Numero1TextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 32 && e.KeyChar <= 44) || (e.KeyChar == 46) || (e.KeyChar == 47) || (e.KeyChar >= 58 && e.KeyChar <= 255))
+            {
+                MessageBox.Show("Solo se aceptan Números Enteros","Alerta", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                e.Handled = true;   
+
+                return;
+            }
+
+
+        }
+
+
+    
+    }
+
+
+}
